a559a451ca68581cec168336f04cf3c2a9bbcb76
twigzxd <twigzx@gmail.com>
2018-11-01 17:59:02 -0400

    nouser

--- carpool-ui/src/app/Modules/Post/postModule.ts
+++ carpool-ui/src/app/Modules/Post/postModule.ts
@@ -2,35 +2,10 @@ import { NgModule } from '@angular/core'
 import { FormsModule }    from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {CommonModule} from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
 
-
-// import {CarpoolOfferComponent} from './Components/carpoolOffer.component';
-// import {PostComponent} from './Components/post.component';
-// import {PageNotFoundComponent} from '../PageNotFound/PageNotFound.component';
-// import {StatusCardComponent} from './Components/statusCard.component';
 import {PostService} from './posts.services';
 import { RoutingPostModule, RoutingComponents } from './RoutingModule';
 
-// const routes :Routes = [
-//     {
-//       path:'carpoolPost',
-//       component:CarpoolOfferComponent
-//     },
-//     {
-//       path: 'post', 
-//       component:PostComponent
-//     },
-//     {
-//       path:'statusCard',
-//       component:StatusCardComponent
-//     }
-//     ,
-//     {
-//       path:'**',
-//       component:PageNotFoundComponent
-//     }
-// ];
 
 @NgModule({
     imports: [
