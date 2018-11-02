19e82f86909c86531da2763e83d89121752c5f17
twigzxd <twigzx@gmail.com>
2018-11-02 11:54:43 -0400

    18/11/1

---
+++ carpool-ui/src/app/Modules/Post/Components/carpoolList.component.ts
@@ -0,0 +1,8 @@
+export class CarpoolListComponent {
+
+
+    let dt = new Date();
+    document.getElementById("Datetime").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ 
+                                                    (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ 
+                                                    (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
+}
