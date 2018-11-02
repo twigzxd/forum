60cd15609a02f8558c939662fb6c125a9947d5b3
twigzxd <twigzx@gmail.com>
2018-11-01 20:24:58 -0400

    18/11/1_8:30

--- carpool-ui/src/app/app.module.ts
+++ carpool-ui/src/app/app.module.ts
@@ -12,9 +12,10 @@ import { RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
   {
-    path: 'test',
+    path: '',
     // loadChildren: () => UserModule
     loadChildren: () => PostModule
+    
   }
 ];
 
